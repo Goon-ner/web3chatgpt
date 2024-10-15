5669369a41ee852a3cb1b21cf746c86e5de8f16e
Mikhail Agapov <mr.hapon@gmail.com>
2024-10-15 17:57:43 +0300

    env

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,15 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
-// import { getAnalytics } from "firebase/analytics";
+import 'dotenv/config'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA0zDYu3Ssgc4f0I6Nf2RumGq3rQU0C9Ds",
-  authDomain: "chatgpt-f9406.firebaseapp.com",
-  projectId: "chatgpt-f9406",
-  storageBucket: "chatgpt-f9406.appspot.com",
-  messagingSenderId: "1046472465213",
-  appId: "1:1046472465213:web:7adf8859bd4265260e8354",
-  measurementId: "G-W6EWW5H1LT"
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId
 };
 
 
