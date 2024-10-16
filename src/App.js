66fca911728722423032bfa34b07e911219193fc
Mikhail Agapov <mr.hapon@gmail.com>
2024-10-16 12:53:42 +0300

    routes

--- src/App.js
+++ src/App.js
@@ -1,101 +1,16 @@
 import './App.css'
-import { useEffect, useState } from 'react';
-import { db } from './firebaseConfig';
-import { collection, getDocs, addDoc, doc, updateDoc, where, query} from 'firebase/firestore';
 import Spinner from 'react-bootstrap/Spinner';
 import HomeIcon from './icons/home.png'
 import FriendsIcon from './icons/friends.png'
 import TasksIcon from './icons/tasks.png'
+import {NavLink, Routes, Route} from 'react-router'
+import {Home} from './Home'
+import {Friends} from './Friends'
+import {Tasks} from './Tasks'
 
 
 function App() {
-  const tg = window.Telegram.WebApp
- 
-
-  const users = collection(db, 'chatgpt');
-  const [points, setPoints] = useState()
-  const [docId, setDocId] = useState()
-  const [started, setStarted] = useState(false)
-  const [timeStart, setTimeStart] = useState()
-  const [counter, setCounter] = useState();
-  const [isLoading, setIsLoading] = useState(true);
   
-  const fetchPoints = async () => {
-    const q = query(users, where('userId', '==', tg.initDataUnsafe.user.id))
-    const querySnapshot = await getDocs(q);
-    if (!querySnapshot.empty) {
-      querySnapshot.forEach((doc) => {
-        setDocId(doc.id)
-        // eslint-disable-next-line no-unused-expressions
-        setPoints(doc.data().points),
-        setTimeStart(doc.data().started)
-        setCounter(14400000 - (new Date().getTime() - doc.data().started))
-      })
-    } else {
-      await addDoc(users, {
-        userId: tg.initDataUnsafe.user.id, 
-        name:tg.initDataUnsafe.user.first_name, 
-        lastname:tg.initDataUnsafe.user.last_name, 
-        points: 5000, 
-        started: 0, 
-        used_tokens: 0,
-        referral: ''
-      })
-      await fetchPoints()
-    }
-  }
-  useEffect(() => {
-    fetchPoints()
-    setIsLoading(false)
-  }, [])
-  useEffect(() => {
-    timeout()
-  }, [counter])
-
-  const handleCatchPoints = async () => {
-    try {
-      const time = new Date().getTime()
-      const update = doc(users, docId)
-      const data = {points: points+1000, started: time}
-      await updateDoc(update, data);
-      setStarted('started')
-      setTimeStart(time)
-      setCounter(14400000)
-      await fetchPoints()
-    } catch (error) {
-      console.error("Error adding action point: ", error);
-    }
-  }
-  const handleStartFarming = async () => {
-    try {
-      const time = new Date().getTime()
-      const update = doc(users, docId)
-      const data = {started: time}
-      await updateDoc(update, data);
-      setStarted('started')
-      setTimeStart(time)
-      setCounter(14400000)
-      await fetchPoints()
-    } catch (error) {
-      console.error(error)
-    }
-  }
-
-  const timeout = () => {
-    const targetTime = timeStart + 14400000
-    setCounter(14400000 - (new Date().getTime() - timeStart))
-    if (!timeStart) {
-      setStarted('notstarted')
-    } else if (targetTime <= new Date().getTime()) {
-      setStarted('stopped')
-    } else setStarted('started')
-  } 
-  const convertTime = (time) => {
-    const hour = Math.floor(time/3600000)
-    const min = Math.floor((time - hour*3600000)/60000)
-    return hour+' Hours '+min+' Minutes'
-  }
-
   return (
     isLoading? 
       <div className='App'>
@@ -107,45 +22,24 @@ function App() {
       </div>
       : 
       <div className="App">
-        <div className='main'>
-          {tg.initDataUnsafe.user.first_name?
-            <h2 className='tgName'>
-          {tg.initDataUnsafe.user.first_name} 
-          {tg.initDataUnsafe.user.last_name}
-          </h2> :
-          <h2>Username</h2>}
-          <div className='balance'>
-            <h2>Your balance:</h2>
-            <h2 >{points} tokens</h2>
-          </div>
-          { started !== 'started' ?
-          <button onClick={
-            started === 'stopped' ? handleCatchPoints : handleStartFarming} 
-            className='farming'>
-            {started === 'stopped' ? 'Claim 1000 tokens' : 'Start farming'}
-            </button> 
-          :
-          <button 
-          disabled={true}
-          className='farming'>
-          {convertTime(counter)}
-          </button>
-          }
-        </div>
-        <div className='space'></div>
-        <div className='footer'>
-          <a className='footer-icons'>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/friends" element={<Friends />}/>
+        <Route path="/tasks" element={<Tasks />}/>
+      </Routes>
+      <div className='footer'>
+          <NavLink to='/' className='footer-icons'>
             <img src={HomeIcon} alt='home icon' className='icon'/>
             <div>Home</div>
-          </a>
-          <a className='footer-icons'>
+          </NavLink>
+          <NavLink to='/friends' className='footer-icons'>
             <img src={FriendsIcon} alt='friends icon' className='icon'/>
             <div>Friends</div>
-          </a>
-          <a className='footer-icons'>
+          </NavLink>
+          <NavLink to='/tasks' className='footer-icons'>
             <img src={TasksIcon} alt='tasks icon' className='icon'/>
             <div>Tasks</div>
-          </a>
+          </NavLink>
         </div>
       </div>
   );
---
+++ src/Home.js
@@ -0,0 +1,125 @@
+import React from 'react'
+import { useEffect, useState } from 'react';
+import { db } from './firebaseConfig';
+import { collection, getDocs, addDoc, doc, updateDoc, where, query} from 'firebase/firestore';
+
+export default function Home() {
+
+    const tg = window.Telegram.WebApp
+ 
+
+  const users = collection(db, 'chatgpt');
+  const [points, setPoints] = useState()
+  const [docId, setDocId] = useState()
+  const [started, setStarted] = useState(false)
+  const [timeStart, setTimeStart] = useState()
+  const [counter, setCounter] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+  
+  const fetchPoints = async () => {
+    const q = query(users, where('userId', '==', tg.initDataUnsafe.user.id))
+    const querySnapshot = await getDocs(q);
+    if (!querySnapshot.empty) {
+      querySnapshot.forEach((doc) => {
+        setDocId(doc.id)
+        // eslint-disable-next-line no-unused-expressions
+        setPoints(doc.data().points),
+        setTimeStart(doc.data().started)
+        setCounter(14400000 - (new Date().getTime() - doc.data().started))
+      })
+    } else {
+      await addDoc(users, {
+        userId: tg.initDataUnsafe.user.id, 
+        name:tg.initDataUnsafe.user.first_name, 
+        lastname:tg.initDataUnsafe.user.last_name, 
+        points: 5000, 
+        started: 0, 
+        used_tokens: 0,
+        referral: ''
+      })
+      await fetchPoints()
+    }
+  }
+  useEffect(() => {
+    fetchPoints()
+    setIsLoading(false)
+  }, [])
+  useEffect(() => {
+    timeout()
+  }, [counter])
+
+  const handleCatchPoints = async () => {
+    try {
+      const time = new Date().getTime()
+      const update = doc(users, docId)
+      const data = {points: points+1000, started: time}
+      await updateDoc(update, data);
+      setStarted('started')
+      setTimeStart(time)
+      setCounter(14400000)
+      await fetchPoints()
+    } catch (error) {
+      console.error("Error adding action point: ", error);
+    }
+  }
+  const handleStartFarming = async () => {
+    try {
+      const time = new Date().getTime()
+      const update = doc(users, docId)
+      const data = {started: time}
+      await updateDoc(update, data);
+      setStarted('started')
+      setTimeStart(time)
+      setCounter(14400000)
+      await fetchPoints()
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const timeout = () => {
+    const targetTime = timeStart + 14400000
+    setCounter(14400000 - (new Date().getTime() - timeStart))
+    if (!timeStart) {
+      setStarted('notstarted')
+    } else if (targetTime <= new Date().getTime()) {
+      setStarted('stopped')
+    } else setStarted('started')
+  } 
+  const convertTime = (time) => {
+    const hour = Math.floor(time/3600000)
+    const min = Math.floor((time - hour*3600000)/60000)
+    return hour+' Hours '+min+' Minutes'
+  }
+
+  return (
+    <div>
+        <div className='main'>
+          {tg.initDataUnsafe.user.first_name?
+            <h2 className='tgName'>
+          {tg.initDataUnsafe.user.first_name} 
+          {tg.initDataUnsafe.user.last_name}
+          </h2> :
+          <h2>Username</h2>}
+          <div className='balance'>
+            <h2>Your balance:</h2>
+            <h2 >{points} tokens</h2>
+          </div>
+          { started !== 'started' ?
+          <button onClick={
+            started === 'stopped' ? handleCatchPoints : handleStartFarming} 
+            className='farming'>
+            {started === 'stopped' ? 'Claim 1000 tokens' : 'Start farming'}
+            </button> 
+          :
+          <button 
+          disabled={true}
+          className='farming'>
+          {convertTime(counter)}
+          </button>
+          }
+        </div>
+        <div className='space'></div>
+    </div>
+  )
+}
